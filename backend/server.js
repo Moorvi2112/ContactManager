1208cd79626b645469e9108f03bd493999241b2e
Moorvi Garg <moorvi0659.be23@chitkara.edu.in>
2026-03-26 21:15:31 +0530

    fixed env

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
 
 // ✅ USE ENV VARIABLE (IMPORTANT FOR RENDER)
 mongoose.connect(process.env.MONGO_URI)
